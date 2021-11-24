355c8800493cefc8309b0b2dfe2e7089831ff019
SizweMalobola <25reyals@gmail.com>
2021-11-24 09:13:21 +0200

    Add rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 
 export default [
   {
     input: "./src/index.js",
     output: [
       {
-        file: "dis/index.js",
+        file: "dist/index.js",
         format: "cjs",
       },
     ],
@@ -23,6 +24,7 @@ export default [
       }),
       external(),
       resolve(),
+      terser(),
     ],
   },
 ];
