e64ea7d8782547d08b39a15a53b65c3c5fc47945
SizweMalobola <25reyals@gmail.com>
2021-11-24 09:08:56 +0200

    Add rollup and rollup configurations

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+
+export default [
+  {
+    input: "./src/index.js",
+    output: [
+      {
+        file: "dis/index.js",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+      }),
+      external(),
+      resolve(),
+    ],
+  },
+];
